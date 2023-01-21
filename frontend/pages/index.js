f19f1412d7e29bc0e20c5453475f36193f994f9a
Suraj Dahal <96937477+surajDahal007@users.noreply.github.com>
2023-01-21 17:10:51 +0545

    index updated with NavBar

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,31 +1,10 @@
+import NavBar from "./NavBar";
+
 export default function Home() {
 
   return (
-    <>
-    <header>
-      <h1 style={{
-        marginTop:"-20px"
-      }}>PAROPAKAR : Blockchain in Charity</h1>
-
-      <div style={{
-        border:"1px solid black",
-        background: "linear-gradient(black,blue)",
-        color:"white"
-      }}>
-        <ul style={{
-          listStyleType: "none",
-          display:"flex",
-          justifyContent: "space-around"
-        }}>
-          <li  style={{cursor:"pointer"}}>User Guidelines</li>
-          <li  style={{cursor:"pointer"}}>Donation Request</li>
-          <li  style={{cursor:"pointer"}}>Tender Log</li>  
-          <li  style={{cursor:"pointer"}}>About Us</li>
-          <li  style={{cursor:"pointer"}}>Media Partner</li>
-        </ul>
-
-      </div>
-    </header>
-    </>
+    <div>
+      <NavBar />
+   </div>
   );
 }
